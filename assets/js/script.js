9c50e55e7a3f1331466c07a11d122e72bc69222f
sshahram <shirin.shahram23@gmail.com>
2021-02-16 14:03:50 -0800

    generate global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// Global variables
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', 
+                        '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']',  
+                        '^', '_', '`', '{', '|', '}', '~'];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 // Assignment code here
 
 
@@ -6,6 +19,31 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  // Ask users about the length of password
+  var numCharacters = window.prompt("How many characters would you like your password to contain?");
+
+  // Check the length of requested password is at least 8 characters
+  if (numCharacters < 8) {
+    window.alert("Your password must be at least 8 characters");
+    writePassword();
+  }
+
+  // Check the length of requested password is no more than 128 characters
+  else if (numCharacters > 128) {
+    window.alert("Your password must be no more than 128 characters");
+    writePassword();
+  }
+
+  // When requested password is between 8 to 128 characters
+  else {
+    var okSpecial = window.confirm("Click OK to confirm including special characters.");
+    if (okSpecial) {
+      var i = (Math.floor(Math.random) * 10) + 22
+      console.log(specialCharacters[i]);
+    }
+
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
