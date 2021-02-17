b3368df77fc30e467055bde7d54062c97e714856
sshahram <shirin.shahram23@gmail.com>
2021-02-17 12:54:32 -0800

    create generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,33 +17,41 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Generate password function
+
+var generatePassword = function() {
+  
+    // Ask users about the length of password
+    var numCharacters = window.prompt("How many characters would you like your password to contain?");
+
+    // Check the length of requested password is at least 8 characters
+    if (numCharacters < 8) {
+      window.alert("Your password must be at least 8 characters");
+      writePassword();
+    }
+  
+    // Check the length of requested password is no more than 128 characters
+    else if (numCharacters > 128) {
+      window.alert("Your password must be no more than 128 characters");
+      writePassword();
+    }
+  
+    // When requested password is between 8 to 128 characters
+    else {
+      var okSpecial = window.confirm("Click OK to confirm including special characters.");
+      if (okSpecial) {
+        var i = (Math.floor(Math.random) * 10) + 22
+        console.log(specialCharacters[i]);
+      }
+  
+    }
+
+};
+
 // Write password to the #password input
 function writePassword() {
 
-  // Ask users about the length of password
-  var numCharacters = window.prompt("How many characters would you like your password to contain?");
 
-  // Check the length of requested password is at least 8 characters
-  if (numCharacters < 8) {
-    window.alert("Your password must be at least 8 characters");
-    writePassword();
-  }
-
-  // Check the length of requested password is no more than 128 characters
-  else if (numCharacters > 128) {
-    window.alert("Your password must be no more than 128 characters");
-    writePassword();
-  }
-
-  // When requested password is between 8 to 128 characters
-  else {
-    var okSpecial = window.confirm("Click OK to confirm including special characters.");
-    if (okSpecial) {
-      var i = (Math.floor(Math.random) * 10) + 22
-      console.log(specialCharacters[i]);
-    }
-
-  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
